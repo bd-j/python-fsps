0d81d092ebfc5c569f747ba94a48978942c03c94
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2014-11-24 15:48:16 -0500

    allow r173 and make a fix for subsequent calls to get_mags with identical parameters returning differnt results

--- fsps/fsps.py
+++ fsps/fsps.py
@@ -603,7 +603,8 @@ class StellarPopulation(object):
 
         NTFULL = driver.get_ntfull()
         NBANDS = driver.get_nbands()
-
+        NSPEC = driver.get_nspec()
+        
         if filter_dict is None:
             filter_dict = FILTERS
         band_array = np.ones(NBANDS, dtype=bool)
@@ -615,7 +616,7 @@ class StellarPopulation(object):
                                 dtype=bool)] = False
 
         inds = np.array(band_array, dtype=int)
-        mags = driver.get_mags(NTFULL, redshift, inds)
+        mags = driver.get_mags(NSPEC, NTFULL, redshift, inds)
 
         if tage > 0.0:
             if bands is not None:
