63ad1e17794b34df57b2f09c81d818c5468bca9b
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2015-06-20 09:20:16 -0400

    changes for rename of sf_theta to sf_trunc, drop support for fsps through SVN, require certain FSPS githash in history

--- fsps/__init__.py
+++ fsps/__init__.py
@@ -21,33 +21,34 @@ def run_command(cmd):
     w = child.wait()
     return os.WEXITSTATUS(w), out
 
-
 # Check to make sure that the required environment variable is present.
 try:
     ev = os.environ["SPS_HOME"]
 except KeyError:
     raise ImportError("You need to have the SPS_HOME environment variable")
 
-# Check the SVN revision number.
-#ACCEPTED_FSPS_REVISIONS = [178, 182, 185, 190, 191]
-#cmd = ["svnversion", ev]
-#stat, out = run_command(" ".join(cmd))
-#fsps_vers = int(re.match("^([0-9])+", out[0]).group(0))
+# Check the githashes to make sure the required FSPS updates are
+# present, and if not or there are no githashes, raise an error
+REQUIRED_GITHASHES = ['6ad1058\n']
 
-# Make sure you don't have some weird mixed version.
-#accepted = ((fsps_vers in ACCEPTED_FSPS_REVISIONS) and
-#            (len(out[0].split(':')) == 1) and
-#            stat == 0)
-accepted = True
+cmd = 'cd {0}; git log --format="format:%h"'.format(ev)
+stat, githashes, err = run_command(cmd)
+accepted = (len(githashes) > 0) and (len(err) == 0)
 if not accepted:
-    raise ImportError("Your FSPS revision, {0}, is not known to work with "
-                      "this version of python-fsps. You can checkout an "
-                      "accepted FSPS revision with "
-                      "'svn update -r rev_number'. "
-                      "The accepted FSPS rev_numbers are: "
-                      "{1}".format(out[0].rstrip('\n'),
-                                   ACCEPTED_FSPS_REVISIONS))
-
+    raise ImportError("Your FSPS version is not under git version "
+                      "control. FSPS is now available on github at "
+                      "https://github.com/cconroy20/fsps")
+accepted = [req in githashes for req in REQUIRED_GITHASHES]
+if not accepted:
+    reqs = ",".join([r[:-2] for r in REQUIRED_GITHASHES])
+    raise ImportError("Your FSPS version does not have correct history.  "
+                      "Please make sure that you have the following commits "
+                      "in your git history: {0}".format(reqs))
+else:
+    # Store the githash.  If any version checking is going to happen,
+    # it should happen here
+    fsps_vers = githashes[0]
+    
 # Only import the module if not run from the setup script.
 try:
     __FSPS_SETUP__
